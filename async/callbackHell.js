ca27b9c2bfe12bb5184d90cb35c9519c94de97cf
Wilmar Caicedo <wilmar.caicedo@farmappweb.com>
2023-09-20 12:14:44 -0500

    callback hell

---
+++ async/callbackHell.js
@@ -0,0 +1,50 @@
+function hola(nombre, miCallback) {
+  setTimeout(() => {
+    console.log(`Hola, ${nombre}`);
+    miCallback(nombre);
+  }, 1500);
+}
+
+function hablar(callbackHablar) {
+  setTimeout(() => {
+    console.log('Bla bla bla bla..');
+    callbackHablar();
+  }, 1000);
+}
+
+function adios(nombre, otroCallback) {
+  setTimeout(() => {
+    console.log(`Adios, ${nombre}`);
+    otroCallback();
+  }, 1000);
+}
+
+function conversacion(nombre, veces, callback) {
+  if (veces > 0) {
+    hablar(() => {
+      conversacion(nombre, --veces, callback);
+    });
+  } else {
+    adios(nombre, callback);
+  }
+}
+
+// --
+
+console.log('Iniciando proceso...');
+hola('Wilmar', (nombre) => {
+  conversacion(nombre, 3, () => {
+    console.log('Proceso terminado');
+  });
+});
+// hola('Wilmar', (nombre) => {
+//   hablar(() => {
+//     hablar(() => {
+//       hablar(() => {
+//         adios(nombre, () => {
+//           console.log('Terminando proceso...');
+//         });
+//       });
+//     });
+//   });
+// });
