15600fcb4bd74f249f8b1e554ca6b2e4d66a5c03
Wilmar Caicedo <wilmar.caicedo@farmappweb.com>
2023-09-21 17:33:07 -0500

    os

---
+++ modulos/os.js
@@ -0,0 +1,28 @@
+const os = require('os');
+
+// console.log(os.arch());
+// console.log(os.platform()); // conocer el sistema operativo sobre el cual esta corriendo mi código
+
+// console.log(os.cpus().length);
+
+// console.log(os.constants);
+
+// const SIZE = 1024;
+// function kb(bytes) { return bytes / SIZE }
+// function mb(bytes) { return kb(bytes) / SIZE }
+// function gb(bytes) { return mb(bytes) / SIZE }
+
+// console.log(os.freemem());
+// console.log(kb(os.freemem()));
+// console.log(mb(os.freemem()));
+// console.log(gb(os.freemem()));
+
+// console.log(gb(os.totalmem()));
+
+// console.log(os.homedir());
+// console.log(os.tmpdir());
+
+// console.log(os.hostname());
+// console.log(os.networkInterfaces());
+
+console.log(os.networkInterfaces());
